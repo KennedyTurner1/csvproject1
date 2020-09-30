1b4dc22af2fe7b81f43fa860ceeed7e149aec39d
John Doe <johndoe@example.com>
2020-09-30 15:42:20 -0500

    finished the project

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Advanced Python - Matplotlib - using CSV files
 this project is to teach students how to use CSV files in python
 this project also teaches students how to visualize the data using matplotlib
 
+I worked to compare high and low temperatures from Death Valley, CA to Sitka, AK during the year of 2018. I wanted to display two different subplots of one figure using matplotlib. 
--- death_valley_and_sitka_comparison.py
+++ death_valley_and_sitka_comparison.py
@@ -18,25 +18,22 @@ header_row_sitka = next(csv_sitka)
 for index, column_header in enumerate(header_row_dv):
     if column_header == 'TMAX': 
         high_dvindex = index
-        print("death valley high", high_dvindex, column_header)
     if column_header == 'TMIN':
         low_dvindex = index
-        print("death valley low", low_dvindex, column_header)
     if column_header == 'DATE':
         date_dvindex = index
-        print("death valley date", date_dvindex, column_header)
+    if column_header == 'NAME':
+        name_dvindex = index
 
 for index, column_header in enumerate(header_row_sitka): 
     if column_header == 'TMAX':
         high_sitkaindex = index
-        print("sitka high", high_sitkaindex, column_header)
     if column_header == 'TMIN':
         low_sitkaindex = index
-        print("sitka low", low_sitkaindex, column_header)
     if column_header == 'DATE':
         date_sitkaindex = index
-        print("sitka date", date_sitkaindex, column_header)
-
+    if column_header == 'NAME':
+        name_sitkaindex = index
 
 #make lists to append to
 highs_dv = []
@@ -49,6 +46,7 @@ lows_sitka = []
 #appending all of the automatic index values to the lists we just created
 for row in csv_dv:
     try:
+        name_dv = row[name_dvindex]
         high_dv = row[high_dvindex]
         low_dv = row[low_dvindex]
         date_dv = datetime.strptime(row[date_dvindex],'%Y-%m-%d')
@@ -62,6 +60,7 @@ for row in csv_dv:
 
 for row in csv_sitka:
     try:
+        name_sitka = row[name_sitkaindex]
         high_sitka = row[high_sitkaindex]
         low_sitka = row[low_sitkaindex]
         date_sitka = datetime.strptime(row[date_sitkaindex],'%Y-%m-%d')
@@ -74,9 +73,6 @@ for row in csv_sitka:
         dates_sitka.append(date_sitka) #append the date to the list
 
 import matplotlib.pyplot as plt #import the graph library as plt, an alias, plot creates a graph
-print(highs_sitka [360:365])
-print(lows_sitka [360:365])
-print(dates_sitka [360:365])
 
 #figure is a plt subplot
 fig, a = plt.subplots(2)
@@ -85,6 +81,7 @@ fig, a = plt.subplots(2)
 a[0].plot(dates_sitka, highs_sitka, c='red') 
 a[0].plot(dates_sitka, lows_sitka, c='blue') 
 
+#fills the gap between both plots
 a[0].fill_between(dates_sitka, highs_sitka, lows_sitka, facecolor='blue', alpha=0.1)
 
 a[1].plot(dates_dv, highs_dv, c='red') 
@@ -92,14 +89,16 @@ a[1].plot(dates_dv, lows_dv, c='blue')
 
 a[1].fill_between(dates_dv, highs_dv, lows_dv, facecolor='blue', alpha=0.1)
 
-#fills the gap between both plots
-
 #chart title
-a[0].set_title("Sitka, AK", fontsize=16)
-a[1].set_title("Death Valley, CA", fontsize=16)
+fig.suptitle((f"Temperature comparison between {name_sitka} and {name_dv}"),fontsize=16)
+
+#subplot title
+a[0].set_title(name_sitka, fontsize=16)
+a[1].set_title(name_dv, fontsize=16)
 
 #the call to fig.autofmt_xdate labels diagonally to prevemt them from overlapping.
 fig.autofmt_xdate()
 
+print("Thanks Professor B! I hope this works.")
 #print statement of subplots
 plt.show()
